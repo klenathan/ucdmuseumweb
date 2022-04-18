2e79eb5a636b6c8dbdf4ac609f3af9a8d3040926
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-04-19 04:43:01 +0700

    added artifacts pages

--- script/artifactListing.js
+++ script/artifactListing.js
@@ -1,33 +1,35 @@
-
-
-for (let i = 0; i<48; i++) {
-    var artifact = document.createElement('a')
-    artifact.setAttribute('href', "artifact/3dview.html");
-    artifact.setAttribute('class', "img-info");
-
-    var image = document.createElement('img')
-    if (i % 4 === 0) {
-        image.src = "assets/artifact/Sculpture “Bust of Róża Loewenfeld”.jpeg"
-    } else {
-        image.src = "assets/artifact/budha_gilt.png"
-    }
-    
-    image.alt = "just a demo picture"
-
-    var artifactName = document.createElement('p')
-    artifactName.innerHTML = 'Lorem ipsum'
-
-    var artifactDate = document.createElement('p')
-    artifactDate.innerHTML = '2022'
-    artifactDate.setAttribute('class', "date");
-
-    artifact.appendChild(image)
-    artifact.appendChild(artifactName)
-    artifact.appendChild(artifactDate)
-
-    document.getElementById("artifact-list").appendChild(artifact);
-}
-
+fetch("../artifact_data.json")
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+        for (var key in data){
+            
+            // console.log(artifact_key)
+            target_href = "artifact/" + key + ".html"
+            var artifact = document.createElement('a')
+            artifact.setAttribute('href', target_href);
+            artifact.setAttribute('class', "img-info");
+        
+            var image = document.createElement('img')
+            image.src = "assets/artifact/budha_gilt.png"
+            
+            image.alt = data[key]["name"]
+        
+            var artifactName = document.createElement('p')
+            artifactName.innerHTML = data[key]["name"]
+        
+            var artifactDate = document.createElement('p')
+            artifactDate.innerHTML = data[key]["date"]
+            artifactDate.setAttribute('class', "date");
+        
+            artifact.appendChild(image)
+            artifact.appendChild(artifactName)
+            artifact.appendChild(artifactDate)
+        
+            document.getElementById("artifact-list").appendChild(artifact);
+        
+        }
+    })
 
 /* <a href="3dview1.html" class="img-info">
         <img
