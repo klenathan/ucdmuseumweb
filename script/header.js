953243aa485250bcf5c6ebed48da965acf26ff66
Nam Thai Tran <66762903+klenathan@users.noreply.github.com>
2022-04-23 15:42:00 +0700

    Fixing nav bar :( still working on search

--- script/header.js
+++ script/header.js
@@ -1,19 +1,19 @@
 let header = `
     <nav>
         <ul class="navbar" id="navbar">
-            <li>
-            <a href="index.html" >
-            <img class="nav-logo" src="assets/logo.png" alt="Logo">
-            </a>
+            <li class="logo-container"> 
+                <a href="index.html" >
+                <img class="nav-logo" src="assets/logo.png" alt="Logo">
+                </a>
             </li>
-            <li><a href="explore.html" class="nav-item">Explore</a></li>
-            <li><a href="artifact.html" class="nav-item">Artifacts</a></li>
+            <li class="nav-item"><a href="explore.html" >Explore</a></li>
+            <li class="nav-item"><a href="artifact.html">Artifacts</a></li>
 
             <li id="search-container">
                 <input id="search_artifact" type="text" placeholder="Search artifact...">
                 <button onclick="searchItem()" id="search-btn" type="submit"><i
                 class="icon-style fa-2x fa-solid fa-magnifying-glass" style="color: #c6b47b;"></i></button>
-                <ul id="seach-result"></ul>
+                <ul id="search-result"></ul>
             </li>
 
             <li><a class="menu-icon" onclick="openMenu()">Menu</a></li>
@@ -37,7 +37,7 @@ input.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
     let userInput = document.getElementById('search_artifact').value;
-    console.log("Searched " + userInput)
+    // console.log("Searched " + userInput)
     // Cancel the default action, if needed
     event.preventDefault();
     // Trigger the button element with a click
@@ -55,36 +55,43 @@ function openMenu() {
     }
 }
 
-resultList =[]
-
 function searchItem() {
+    resultList = []
+    document.getElementById("search-result").innerHTML = ''
     fetch("artifact_data.json")
         .then(res => res.json())
         .then(data => {
-            queryResult = "<ul></ul>"
             Object.keys(data).forEach(function (key) {
                 let userInput = document.getElementById('search_artifact').value;
-                // console.log("Searched " + userInput)
                 let target = key.toLowerCase().split("_")
-                // console.log(target)
                 Object.keys(target).forEach(function (k) {
                     if (target[k].includes(userInput)) {
-                        let result = target[k]["name"]
-                        console.log(target[k]["name"])
+                        let result = data[key]["name"]
                         resultList.push(result)
                         resultList = [...new Set(resultList)]
-                        // console.log(resultList)
-                        for(v in resultList) {
-                            var eraName = document.createElement('p')
-                            eraName.innerHTML = resultList[v]
-                            document.getElementById("seach-result").appendChild(eraName)
-                        }
                         // window.location.href = "artifact/" + target.join("_") + ".html";
                     }
                     else {
                         // console.log("cannot find")
                     }
                 })
+                // console.log(userInput === "")
+                console.log(resultList === [])
+                if (userInput === "" || userInput === " ") {
+                    resultList = ["Result not found"]
+                } else if (resultList === []) {
+                    resultList = ["Result not found"]
+                }
+                
+                
             });
+
+            for(v in resultList) {
+                // console.log(resultList[v])
+                var resultElement = document.createElement('ul')
+                resultElement.innerHTML = resultList[v]
+                resultElement.setAttribute('class', "search-result-element")
+                document.getElementById("search-result").appendChild(resultElement)
+            }
         })
 }
