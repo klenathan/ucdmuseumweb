14e99dfc95efc544143b0824931da44415892ad5
ThuPham02 <phamthu10602@gmail.com>
2022-04-26 21:03:45 +0700

    add content

--- explore/newHeader.js
+++ explore/newHeader.js
@@ -10,9 +10,10 @@ let newHeader = `
             <li class="nav-item"><a href="../artifact.html">Artifacts</a></li>
 
             <li id="search-container">
-                <input id="search_artifact" type="text" placeholder="Search artifact...">
+                
                 <button id="search-btn" type="submit"><i
                 class="icon-style fa-2x fa-solid fa-magnifying-glass" style="color: #c6b47b;"></i></button>
+                <input id="search_artifact" type="text" placeholder="Search artifact...">
                 <ul id="search-result"></ul>
             </li>
 
@@ -77,7 +78,7 @@ function searchItem() {
                 })
             });
 
-
+            
             // console.log(userInput === "")
             if (userInput === "" || userInput === " ") {
                 resultList = []
