e4bae94a10b1fa17775e904b182317efe875ba62
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-05-19 15:39:05 +0700

    Format code

--- script/artifactListing.js
+++ script/artifactListing.js
@@ -1,35 +1,33 @@
 fetch("artifact_data.json")
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data)
-        for (var key in data){
-            
-            // console.log(artifact_key)
-            target_href = "artifact/" + key.toLowerCase() + ".html"
-            var artifact = document.createElement('a')
-            artifact.setAttribute('href', target_href);
-            artifact.setAttribute('class', "img-info");
-        
-            var image = document.createElement('img')
-            image.src = "assets/artifact/"+key+".png"
-            
-            image.alt = data[key]["name"]
-        
-            var artifactName = document.createElement('p')
-            artifactName.innerHTML = data[key]["name"]
-        
-            var artifactDate = document.createElement('p')
-            artifactDate.innerHTML = data[key]["date"]
-            artifactDate.setAttribute('class', "date");
-        
-            artifact.appendChild(image)
-            artifact.appendChild(artifactName)
-            artifact.appendChild(artifactDate)
-        
-            document.getElementById("artifact-list").appendChild(artifact);
-        
-        }
-    })
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data)
+    for (var key in data) {
+      // console.log(artifact_key)
+      target_href = "artifact/" + key.toLowerCase() + ".html";
+      var artifact = document.createElement("a");
+      artifact.setAttribute("href", target_href);
+      artifact.setAttribute("class", "img-info");
+
+      var image = document.createElement("img");
+      image.src = "assets/artifact/" + key + ".png";
+
+      image.alt = data[key]["name"];
+
+      var artifactName = document.createElement("p");
+      artifactName.innerHTML = data[key]["name"];
+
+      var artifactDate = document.createElement("p");
+      artifactDate.innerHTML = data[key]["date"];
+      artifactDate.setAttribute("class", "date");
+
+      artifact.appendChild(image);
+      artifact.appendChild(artifactName);
+      artifact.appendChild(artifactDate);
+
+      document.getElementById("artifact-list").appendChild(artifact);
+    }
+  });
 
 /* <a href="3dview1.html" class="img-info">
         <img
--- script/eraListing.js
+++ script/eraListing.js
@@ -1,37 +1,27 @@
-
 let eraList = ["Chinese", "Korean", "Japanese"];
 
-for (let i = 0; i<eraList.length; i++) {
-    var era = document.createElement('a')
-    let target = "explore/" + eraList[i].toLowerCase()  + ".html"
-    era.setAttribute('href', target);
-    era.setAttribute('class', "img-info");
-    era.alt = "Redirect"
+for (let i = 0; i < eraList.length; i++) {
+  var era = document.createElement("a");
+  let target = "explore/" + eraList[i].toLowerCase() + ".html";
+  era.setAttribute("href", target);
+  era.setAttribute("class", "img-info");
+  era.alt = "Redirect";
 
-    var image = document.createElement('img')
-    if (eraList[i] === "Chinese") {
-        image.src = "assets/terracotta_army.jpg"
-    } else if (eraList[i] === "Korean") {
-        image.src = "assets/korean_palace.jpg"
-    } else {
-        image.src = "assets/fuji_mount.jpg"
-    }
-    image.alt = "just a demo picture"
+  var image = document.createElement("img");
+  if (eraList[i] === "Chinese") {
+    image.src = "assets/terracotta_army.jpg";
+  } else if (eraList[i] === "Korean") {
+    image.src = "assets/korean_palace.jpg";
+  } else {
+    image.src = "assets/fuji_mount.jpg";
+  }
+  image.alt = "just a demo picture";
 
-    var eraName = document.createElement('p')
-    eraName.innerHTML = eraList[i]
+  var eraName = document.createElement("p");
+  eraName.innerHTML = eraList[i];
 
-    era.appendChild(image)
-    era.appendChild(eraName)
+  era.appendChild(image);
+  era.appendChild(eraName);
 
-    document.getElementById("era-list").appendChild(era);
+  document.getElementById("era-list").appendChild(era);
 }
-
-/* <a href="3dview1.html" class="img-info">
-        <img
-        src="assets/artifact/Sculpture “Bust of Róża Loewenfeld”.jpeg"
-        alt=""
-        />'
-        <p>Lorem ipsum</p> 
-    </a>
-*/
